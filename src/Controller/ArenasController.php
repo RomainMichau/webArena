d5b757092b746919277873af11affe776324fe5c
RomainMichau <romain.michau@edu.ece.fr>
2017-10-21 15:40:24 +0200

    embellissement tableau

--- src/Controller/ArenasController.php
+++ src/Controller/ArenasController.php
@@ -34,21 +34,25 @@ class ArenasController extends AppController {
         $this->loadModel('Fighters');
         $this->loadModel('Surroundings');
         
-        for ($i = 0; $i < 15; $i++) {
-            for ($j = 0; $j < 10; $j++) {
+        for ($i = 1; $i <= 15; $i++) {
+            for ($j = 1 ; $j <= 10; $j++) {
                 if ($this->Fighters->getFighterByCoord($i, $j) != NULL) {
-                    $tab[$i][$j] = $this->Fighters->getFighterByCoord($i, $j)->id;
+                    $tab[$i][$j] = 'f'.$this->Fighters->getFighterByCoord($i, $j)->id;
+                  //  pr($tab[$i][$j]);
                 } elseif($this->Surroundings->getSurroundingByCoord($i, $j)!=NULL) {
-                    $tab[$i][$j] = $this->Surroundings->getSurroundingByCoord($i, $j)->id;
+                    $tab[$i][$j] = 's'.$this->Surroundings->getSurroundingByCoord($i, $j)->id;
                 }
-                else{$tab[$i][$j]="0";}
+                else{$tab[$i][$j]='vide';}
             }
         }
       $this->set('tab',$tab);
+   //   $this->tst();
     }
 
     public function diary() {
         $this->set('titredepage', "diary");
     }
+    
+    public function tst(){pr("oki");}
 
 }
--- src/Template/Arenas/sight.ctp
+++ src/Template/Arenas/sight.ctp
@@ -8,11 +8,19 @@ $this->assign('title', $titredepage);?>
             <?php foreach ($value1 as $value2) { ?>     
             <td class='case'>
                 <?php 
-                    if($value2!=0){
-                        echo $this->Html->image($value2.'.png', ['alt' => 'CakePHP']);
-                    }?>
+                 
+                            echo $this->Html->image($value2.'.png', ['alt' => 'imgNotFound','width'=>'42','height'=>'42']);
+                    ?>
             </td>           
             <?php } ?>
         </tr>
     <?php } ?>
 </table>
+<?php
+$this->Form->button(
+    'Click me', 
+   
+            array('controller' => 'users','action' => 'sight' )
+         
+    
+);
