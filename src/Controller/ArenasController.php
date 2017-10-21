ee2b9640dff8eee591160e8a1c18278197633be9
RomainMichau <romain.michau@edu.ece.fr>
2017-10-21 14:58:13 +0200

    ajout affichage image dans le sight

--- src/Controller/ArenasController.php
+++ src/Controller/ArenasController.php
@@ -32,17 +32,19 @@ class ArenasController extends AppController {
         
         $this->set('titredepage', "sight");
         $this->loadModel('Fighters');
-
+        $this->loadModel('Surroundings');
+        
         for ($i = 0; $i < 15; $i++) {
             for ($j = 0; $j < 10; $j++) {
                 if ($this->Fighters->getFighterByCoord($i, $j) != NULL) {
-                    $tab[$i][$j] = $this->Fighters->getFighterByCoord($i, $j);
-                } else {
-                    $tab[$i][$j] = 2;
+                    $tab[$i][$j] = $this->Fighters->getFighterByCoord($i, $j)->id;
+                } elseif($this->Surroundings->getSurroundingByCoord($i, $j)!=NULL) {
+                    $tab[$i][$j] = $this->Surroundings->getSurroundingByCoord($i, $j)->id;
                 }
+                else{$tab[$i][$j]="0";}
             }
         }
-      //  pr($tab);
+      $this->set('tab',$tab);
     }
 
     public function diary() {
--- src/Template/Arenas/sight.ctp
+++ src/Template/Arenas/sight.ctp
@@ -1,1 +1,18 @@
-<?php $this->assign('title', $titredepage);?>
+<?php
+use Cake\View\Helper\HtmlHelper;
+
+$this->assign('title', $titredepage);?>
+<table class='damier'> 
+    <?php foreach ($tab as $value1) { ?>
+        <tr> 
+            <?php foreach ($value1 as $value2) { ?>     
+            <td class='case'>
+                <?php 
+                    if($value2!=0){
+                        echo $this->Html->image($value2.'.png', ['alt' => 'CakePHP']);
+                    }?>
+            </td>           
+            <?php } ?>
+        </tr>
+    <?php } ?>
+</table>
