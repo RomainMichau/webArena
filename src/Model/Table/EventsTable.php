e04c5b146a7eea3e67d511d28375fe9d8e50741d
Vincent Jacob <vincent.jacob@edu.ece.fr>
2017-10-25 20:31:30 +0200

    Nettoyage EventsTable

--- src/Model/Table/EventsTable.php
+++ src/Model/Table/EventsTable.php
@@ -11,10 +11,7 @@ use Cake\ORM\Table;
  * @author romai
  */
 class EventsTable extends Table {
-    //put your code here
-    public function tst(){
-    return("oki");
-    }
+
     public  function addEvent($name,$x,$y){
         date_default_timezone_set('Europe/Paris');
        $event= $this->newEntity();
@@ -23,19 +20,15 @@ class EventsTable extends Table {
         $event->coordinate_x=$x;
         $event->coordinate_y=$y;
         $event->date=$d;
-        $this->save($event);
-        
-              
-        
-        
+        $this->save($event);    
     }
 
     /**
-     * [getVisibleLatestEvents description]
-     * @param  [type] $fighter_x   [description]
-     * @param  [type] $fighter_y   [description]
-     * @param  [type] $sight_skill [description]
-     * @return [type]              [description]
+     * [getVisibleLatestEvents]
+     * @param  [int] $fighter_x   [fighter x pos]
+     * @param  [int] $fighter_y   [fighter y pos]
+     * @param  [int] $sight_skill [number of sight skill]
+     * @return [visible recent events] [null if none]
      */
     public function getVisibleLatestEvents($fighter_x, $fighter_y, $sight_skill)
     {
@@ -53,22 +46,11 @@ class EventsTable extends Table {
         $query->select(['date', 'x' => 'coordinate_x', 'y' => 'coordinate_y', 'name'])
         ->where(function ($exp, $q) use ($start_date, $end_date) { return $exp->between('date', $start_date, $end_date); });
 
+        // Event visible if |fighter_x - event_x| + |fighter_y - event_y| <= sight_skill
         $man_dist = $query->newExpr('ABS(' . $fighter_x . '-coordinate_x) + ABS(' . $fighter_y . '-coordinate_y)');
         $query->andWhere([$sight_skill . ' >=' => $man_dist]);
-
-        /*
-        ->andWhere(function ($exp, $q) use ($fighter_x, $fighter_y, $sight_skill) { 
-            $dist_x = abs($fighter_x - coordinate_x);
-            $dist_y = abs($fighter_y - coordinate_y);
-        }); */
-
-        // [(abs($fighter_x - coordinate_x) + abs($fighter_y - coordinate_y)) . ' <=' => $sight_skill . '']
-        //->andWhere('' => $sight_skill);
         
-        // Execution of query and return of result
-        //$query->hydrate(false);
-        //$result = $query->toList();
-        
+        // Query execution and return
         return ($query->isEmpty()) ? null : $query->toArray();
     }
 }
